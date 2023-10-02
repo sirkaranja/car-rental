79de8d46fd616da9f76002eeb4f7849535ba0449
Daniel Karanja <sirkaranja12@gmail.com>
2023-10-02 21:52:01 +0300

    database

--- server/app.py
+++ server/app.py
@@ -5,16 +5,14 @@ from flask_migrate import Migrate
 app = Flask(__name__)
 
 # Configure your database connection URL
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///your_database.db'  # Use your preferred database URL here
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Disable Flask-SQLAlchemy modification tracking
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///carhire.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  
 
 # Initialize the database
 db.init_app(app)
-
-# Initialize database migrations (optional but recommended)
 migrate = Migrate(app, db)
 
-# Import your routes and views here (e.g., create routes for your car rental application)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
