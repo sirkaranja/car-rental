0d5dea1e0c3497124b40b57f846548756e78bb4c
Daniel Karanja <sirkaranja12@gmail.com>
2023-10-02 22:07:34 +0300

    route for fetching users

--- server/app.py
+++ server/app.py
@@ -1,5 +1,5 @@
-from flask import Flask
-from models import db
+from flask import Flask, jsonify
+from models import db, User, Booking, Car, Payment
 from flask_migrate import Migrate
 
 app = Flask(__name__)
@@ -12,6 +12,28 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 migrate = Migrate(app, db)
 
+
+@app.route('/')
+def home():
+    return "<h2>Dashboard,/h2>"
+
+
+app.route('/users', methods=['GET'])
+def get_all_users():
+    users = User.query.all()
+    users_list=[]
+    for user in users:
+        try:
+            user_data={
+                'id': user.id,
+                'name': user.name,
+                'email': user.email,
+                'role': user.role,
+            }
+            users_list.append(user_data)
+        except Exception as e:
+            print (f"Error processing users list {user.id}: {e}")
+    return jsonify(users_list)
 
 
 if __name__ == '__main__':
