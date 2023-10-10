1427342b2dd1f4bcd8e07c86dda1f4ef78c6d446
Daniel Karanja <sirkaranja12@gmail.com>
2023-10-10 23:37:53 +0300

    navbar import

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import NavbaMain from '../src/Components/Navbar/Navbar'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-<h3>CH Carhire System</h3>
+    < NavbaMain />
+
   )
 }
 
