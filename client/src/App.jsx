499cb2f593232ddf9de712d3f2741f4d146b82a9
Daniel Karanja <sirkaranja12@gmail.com>
2023-10-11 22:16:36 +0300

    Coursel component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import NavbaMain from '../src/Components/Navbar/Navbar'
-
+import Coursel from '../src/Components/Coursel/coursel'
 
 function App() {
 
 
   return (
-    < NavbaMain />
+    <><NavbaMain />
+    <Coursel /></>
 
   )
 }
---
+++ client/src/Components/Coursel/coursel.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './coursel.css'
+function Coursel() {
+  return (
+    <div className='coursel'>
+
+
+
+    </div>
+  )
+}
+
+export default Coursel
