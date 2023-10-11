7a7dcb93f656c55bb90fca9fd9e06a8a92528b00
Daniel Karanja <sirkaranja12@gmail.com>
2023-10-11 22:22:10 +0300

    main- Coursel content

--- client/src/Components/Coursel/coursel.jsx
+++ client/src/Components/Coursel/coursel.jsx
@@ -3,8 +3,11 @@ import './coursel.css'
 function Coursel() {
   return (
     <div className='coursel'>
-
-
+    <div className='main-coursel'>
+    <h3>Car Rental Is stressful </h3>
+    <h5>say hello to affordable  car sharing with no red tape</h5>
+    </div>
+ 
 
     </div>
   )
