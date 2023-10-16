294df871309a12116261aba14d022702decc4be3
Daniel Karanja <sirkaranja12@gmail.com>
2023-10-16 22:30:17 +0300

    About component

--- client/src/Components/About/about.jsx
+++ client/src/Components/About/about.jsx
@@ -1,10 +1,37 @@
 import React from 'react'
-import Footer from '../src/Components/Footer/Footer'
-import NavbaMain from '../src/Components/Navbar/Navbar'
+import Footer from '../Footer/Footer';
+import NavbaMain from '../Navbar/Navbar'
+import './about.css'
+import ford from '../../assets/frd.png'
+import more from '../../assets/more.png'
 
 function About() {
   return (
-  <><NavbaMain /><Footer /></>
+
+    <>
+    <NavbaMain />
+
+    <div className='mainpart'>
+      <div className='header'>
+        About
+      </div>
+      </div>
+      <div>
+        <img src={ford} alt="" srcset="" />
+        <p>A Ford Ranger rental comes with a high-strength steel frame for<br/>
+ extra durability and exceptional gas mileage for its class.</p>
+ <div className='car-details'>
+  <h3>Details</h3>
+ </div>
+ <div className='more-cars'>
+  <img src={more} alt="" srcset="" className='img-more'/>
+ </div>
+ <h5>View more Categories</h5>
+ <button>More cars</button>
+      </div> 
+      
+    <Footer />
+    </>
   )
 }
 
