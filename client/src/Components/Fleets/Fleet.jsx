4791efd0f7d9f092083aa39310a44926cceb6344
Daniel Karanja <sirkaranja12@gmail.com>
2023-10-30 22:26:04 +0300

    fetch method deleted

--- client/src/Components/Fleets/Fleet.jsx
+++ client/src/Components/Fleets/Fleet.jsx
@@ -4,38 +4,13 @@ import NavbarMain from '../Navbar/Navbar';
 import './fleet.css';
 
 function Fleet() {
-  const [cars, setCars] = useState([]);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    // Fetch car data from your backend API (replace with your API endpoint)
-    fetch('/api/cars') // Update the URL to your API endpoint
-      .then((response) => response.json())
-      .then((data) => {
-        setCars(data);
-      })
-      .catch((error) => {
-        console.error('Error fetching car data:', error);
-        setError('An error occurred while fetching car data.');
-      });
-  }, []);
+  
 
   return (
     <>
       <NavbarMain />
       <div className='header'>
         <h3>Our Fleet</h3>
-      </div>
-      <div className="car-list">
-        {error ? (
-          <p>{error}</p>
-        ) : (
-          cars.map((car) => (
-            <div key={car.id} className="car-card">
-              {/* Your car card content here */}
-            </div>
-          ))
-        )}
       </div>
       <Footer />
     </>
