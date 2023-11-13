d49bbdc0867430293049933fa96a0fd1a83e6225
Daniel Karanja <sirkaranja12@gmail.com>
2023-11-13 21:39:29 +0300

    added html semantic and form improvement

--- client/src/Components/Contact/Contact.jsx
+++ client/src/Components/Contact/Contact.jsx
@@ -1,44 +1,39 @@
-import React from 'react'
+import React from 'react';
 import Footer from '../Footer/Footer';
-import NavbaMain from '../Navbar/Navbar'
-import './contact.css'
+import NavbarMain from '../Navbar/Navbar';
+import './contact.css';
 
 function Contact() {
   return (
     <>
-    < NavbaMain />
-  {/* contact form */}
-    <div className='header'>Contact</div>
-    <div className='main-contact'>
+      <NavbarMain />
+      {/* Contact form */}
+      <div className='header'>Contact</div>
+      <section className='main-contact'>
         <form action="">
-            <label htmlFor="">Name</label>
-            <input type="text" placeholder='name'/>
-            <label htmlFor="">Email</label>
-            <input type="email" name="email"  placeholder='email....' id="" />
-            <label htmlFor="">Subject</label>
-            <input type="text" placeholder='subject...' />
-            <label htmlFor="">Phone Number</label>
-            <input type="number" name="phone number..." id="" />
-            <textarea name="" id="" cols="30" rows="10"></textarea>
-            <button type="submit">Send Message</button>
+          <label htmlFor="name">Name</label>
+          <input type="text" id="name" placeholder='Name' />
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" placeholder='Email' />
+          <label htmlFor="subject">Subject</label>
+          <input type="text" id="subject" placeholder='Subject' />
+          <label htmlFor="phone">Phone Number</label>
+          <input type="tel" id="phone" placeholder='Phone Number' />
+          <textarea id="message" cols="30" rows="10" placeholder='Your message'></textarea>
+          <button type="submit">Send Message</button>
         </form>
-<div>
-<h3>Contact Information</h3>
-        <p>Nairobi, Kenya</p><br/>
-<p>Email us at: <email>info@carhire.co.ke</email></p>
-<p>Call : 0798343427</p>
-<p>Follow us on our socials</p>
-</div>
-        
-      
-
-    </div>
-    <div className='ftr'>
-
-    </div>
-    < Footer />
+        <div>
+          <h3>Contact Information</h3>
+          <p>Nairobi, Kenya</p>
+          <p>Email us at: <span className="email">info@carhire.co.ke</span></p>
+          <p>Call: 0798343427</p>
+          <p>Follow us on our socials</p>
+        </div>
+      </section>
+      <div className='ftr'></div>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default Contact
+export default Contact;
