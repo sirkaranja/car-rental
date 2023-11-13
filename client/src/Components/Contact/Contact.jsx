95f5b962718dc3389d4b9562470bce17bc6b1309
Daniel Karanja <sirkaranja12@gmail.com>
2023-11-13 21:49:15 +0300

    form input flex display

--- client/src/Components/Contact/Contact.jsx
+++ client/src/Components/Contact/Contact.jsx
@@ -8,28 +8,43 @@ function Contact() {
     <>
       <NavbarMain />
       {/* Contact form */}
-      <div className='header'>Contact</div>
-      <section className='main-contact'>
-        <form action="">
-          <label htmlFor="name">Name</label>
-          <input type="text" id="name" placeholder='Name' />
-          <label htmlFor="email">Email</label>
-          <input type="email" id="email" placeholder='Email' />
-          <label htmlFor="subject">Subject</label>
-          <input type="text" id="subject" placeholder='Subject' />
-          <label htmlFor="phone">Phone Number</label>
-          <input type="tel" id="phone" placeholder='Phone Number' />
-          <textarea id="message" cols="30" rows="10" placeholder='Your message'></textarea>
-          <button type="submit">Send Message</button>
-        </form>
-        <div>
-          <h3>Contact Information</h3>
-          <p>Nairobi, Kenya</p>
-          <p>Email us at: <span className="email">info@carhire.co.ke</span></p>
-          <p>Call: 0798343427</p>
-          <p>Follow us on our socials</p>
-        </div>
-      </section>
+      <div className='main-contact'>
+  <form action="">
+    <div className="flex-container">
+      <div className="flex-item">
+        <label htmlFor="name">Name</label>
+        <input type="text" id="name" placeholder='Name' />
+      </div>
+      <div className="flex-item">
+        <label htmlFor="email">Email</label>
+        <input type="email" id="email" placeholder='Email' />
+      </div>
+    </div>
+
+    <div className="flex-container">
+      <div className="flex-item">
+        <label htmlFor="subject">Subject</label>
+        <input type="text" id="subject" placeholder='Subject' />
+      </div>
+      <div className="flex-item">
+        <label htmlFor="phone">Phone Number</label>
+        <input type="tel" id="phone" placeholder='Phone Number' />
+      </div>
+    </div>
+
+    <textarea id="message" cols="30" rows="10" placeholder='Your message'></textarea>
+    <button type="submit">Send Message</button>
+  </form>
+
+  <div className='contact-info'>
+    <h3>Contact Information</h3>
+    <p>Nairobi, Kenya</p>
+    <p>Email us at: <span className="email">info@carhire.co.ke</span></p>
+    <p>Call: 0798343427</p>
+    <p>Follow us on our socials</p>
+  </div>
+</div>
+
       <div className='ftr'></div>
       <Footer />
     </>
