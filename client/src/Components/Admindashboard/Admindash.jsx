da48e1db44f863f44c4d3e18fc4775ab6272cc6e
Daniel Karanja <sirkaranja12@gmail.com>
2023-11-25 23:10:54 +0300

    welcome message

--- client/src/Components/Admindashboard/Admindash.jsx
+++ client/src/Components/Admindashboard/Admindash.jsx
@@ -56,8 +56,19 @@ function Admindash() {
         </div>
         <div class="col py-3">
             <h3>Left Sidebar with Submenus</h3>
+            <form class="form-inline my-2 my-lg-0">
+      < input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit"></button>
+    </form>
+    <div className='msg'>
+<h4>Welcome back, Admin!</h4>
+<p>Today is September 22, 2023. You have 126 news and alerts,<br /> along with 3 messages awaiting your response.</p>
+
         </div>
+        </div>
+       
     </div>
+    
 </div>
   )
 }
