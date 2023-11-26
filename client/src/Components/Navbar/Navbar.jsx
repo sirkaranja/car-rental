9d0f70cc6b05a53f25b1a0b806de7ea6c6eaa0ba
Daniel Karanja <sirkaranja12@gmail.com>
2023-11-26 18:18:28 +0300

    navbar linking with browser route

--- client/src/Components/Navbar/Navbar.jsx
+++ client/src/Components/Navbar/Navbar.jsx
@@ -15,9 +15,9 @@ function NavbaMain() {
           <Nav className="me-auto">
             <Nav.Link href="/">Home</Nav.Link>
             <Nav.Link href="about">About</Nav.Link>
-            <Nav.Link href="/fleet">Our fleets</Nav.Link>
-            <Nav.Link href="/services">Services</Nav.Link>
-            <Nav.Link href="/contact">Contact</Nav.Link>
+            <Nav.Link href="fleet">Our fleets</Nav.Link>
+            <Nav.Link href="services">Services</Nav.Link>
+            <Nav.Link href="contact">Contact</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
